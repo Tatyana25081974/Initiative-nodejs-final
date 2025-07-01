798fd46ed8940025660255e309f196b01addc071
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2025-07-01 19:59:45 +0200

    fix 404 error

--- src/services/categoriesService.js
+++ src/services/categoriesService.js
@@ -4,7 +4,7 @@ import { Category } from '../db/models/categoriesModel.js';
 export const categoriesService = async () => {
   const categories = await Category.find();
 
-  if (!categories) {
+  if (categories.length === 0) {
     throw createHttpError(404, 'Categories not found');
   }
 
